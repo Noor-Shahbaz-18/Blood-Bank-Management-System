c29ed80762a63f69bfa13222dcdfbf37800427b2
Noor-Shahbaz-18 <fatimanoor21510@gmail.com>
2026-06-12 13:23:38 +0500

    Add files via upload

---
+++ BloodInventoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using BloodBankManagementSystem.Classes.Database;
+using BloodBankManagementSystem.Classes.Models;
+
+namespace BloodBankManagementSystem.Classes.BusinessLogic
+{
+    public class BloodInventoryManager
+    {
+        public static DataTable GetAllBloodBags()
+        {
+            // Will connect to BloodBagDAL
+            return new DataTable();
+        }
+
+        public static bool AddBloodBag(BloodBag bag)
+        {
+            if (bag == null) return false;
+            // Validation
+            if (string.IsNullOrEmpty(bag.BagID)) return false;
+            if (string.IsNullOrEmpty(bag.BloodGroup)) return false;
+
+            // Save to database
+            return BloodBagDAL.Insert(bag);
+        }
+
+        public static bool UpdateBloodBag(BloodBag bag)
+        {
+            if (bag == null) return false;
+            return BloodBagDAL.Update(bag);
+        }
+
+        public static bool DeleteBloodBag(string bagID)
+        {
+            return BloodBagDAL.Delete(bagID);
+        }
+
+        public static BloodBag GetBloodBagByID(string bagID)
+        {
+            return BloodBagDAL.GetByID(bagID);
+        }
+
+        public static DataTable GetBloodBagsByGroup(string bloodGroup)
+        {
+            return BloodBagDAL.GetByBloodGroup(bloodGroup);
+        }
+
+        public static int GetAvailableUnits(string bloodGroup)
+        {
+            return BloodBagDAL.GetAvailableCount(bloodGroup);
+        }
+
+        public static bool UpdateStockStatus(string bagID, string status)
+        {
+            return BloodBagDAL.UpdateStatus(bagID, status);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BloodBankManagementSystem")]
+[assembly: AssemblyDescription("Blood Bank Management System")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BloodBankManagementSystem")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("458ee92a-832c-4ffc-872b-cbdd0f723118")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
